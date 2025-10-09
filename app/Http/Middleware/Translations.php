29944f0a679f086baad79059185f074c38d0990e
Alfredo Castillejos <alfredo.castillejos@kaohoteles.com>
2025-10-09 16:59:41 -0600

    Test var 10

--- app/Http/Middleware/Translations.php
+++ app/Http/Middleware/Translations.php
@@ -23,6 +23,7 @@ class Translations
     {
         $group_lang = $request->route()->getPrefix();
 	    //$group_locale_clean = substr($group_lang, 1,2);
+        echo "X:".$group_lang;
         $group_locale_clean = mb_substr($group_lang, 1, 2, 'UTF-8');
         //echo "X:".$group_locale_clean;
         app()->setLocale($this->languages[$group_locale_clean]);
