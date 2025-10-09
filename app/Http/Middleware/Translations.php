0a11c83c7f513e8477b6b9fe4eed5200835b8ca9
Alfredo Castillejos <acastillejos@dixi-project.com>
2025-10-09 09:25:16 -0600

    Ajustes

--- app/Http/Middleware/Translations.php
+++ app/Http/Middleware/Translations.php
@@ -22,7 +22,7 @@ class Translations
     public function handle(Request $request, Closure $next)
     {
         $group_lang = $request->route()->getPrefix();
-        $group_locale_clean = substr($group_lang, 1);
+	$group_locale_clean = substr($group_lang, 0,2);
         app()->setLocale($this->languages[$group_locale_clean]);
         return $next($request);
     }
