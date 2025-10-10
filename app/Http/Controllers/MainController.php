4813fafb16a84dfebba05a491f73f59f6bb5a630
Alfredo Castillejos <alfredo.castillejos@kaohoteles.com>
2025-10-10 10:08:05 -0600

    Test 16..

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -40,9 +40,7 @@ class MainController extends Controller
     public function tools(Request $request, $tab = '1', Continent $continent = null,  $regionid = null, $type = null) {
         echo $tab."-".$continent."-".$regionid."-".$type."<br>";
         // Variables globales $type
-
-
-     
+        
         if (!Auth::check()) {
             return redirect(route(__('routes.home')));
         }
